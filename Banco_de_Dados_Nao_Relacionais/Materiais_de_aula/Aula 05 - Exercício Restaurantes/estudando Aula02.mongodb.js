6155a3ff0a4292aea2a8a58e537a387c34a4cba5
Rafael Martins Cardoso <r17cardoso@hotmail.com>
2024-09-30 14:47:57 -0300

    estudando mongoDB

---
+++ Banco_de_Dados_Nao_Relacionais/Materiais_de_aula/Aula 05 - Exercício Restaurantes/estudando Aula02.mongodb.js
@@ -0,0 +1,4 @@
+/* Antes de criar um banco de dados eu primeiramente tive que conectar no localhost:27017*/
+
+//Comando usado para acessar o banco de dados, caso o banco de dados nao exista o mesmo sera criado
+use('estudandoAula02') 
