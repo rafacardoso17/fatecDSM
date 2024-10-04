69a269793e856834bde662cba0bedce60ece765f
Rafael_Deb <r17cardoso@hotmail.com>
2024-10-03 21:54:27 -0300

    "Criado db FoodClub para criar o MER"

--- Desenvolvimento_web_II/DSWII_project_01/src/app/main/main.component.ts
+++ Desenvolvimento_web_II/DSWII_project_01/src/app/main/main.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
 import { CategoryBarComponent } from "../category-bar/category-bar.component";
+import { CoursesComponent } from "../courses/courses.component";
+import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent, NavBarComponent, CategoryBarComponent],
+  imports: [HeaderComponent, NavBarComponent, CategoryBarComponent, CoursesComponent, RouterOutlet],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
--- Desenvolvimento_web_II/DSWII_project_01/src/app/models/courses.ts
+++ Desenvolvimento_web_II/DSWII_project_01/src/app/models/courses.ts
@@ -6,5 +6,4 @@ export class Course {
   cost: number = 0;
   image: string = '';
   duration: string = '';
-
 }
